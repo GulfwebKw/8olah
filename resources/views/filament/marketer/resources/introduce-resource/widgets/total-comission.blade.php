f203db26beb15382809f5c13ac07292cd4fe4cc0
Erfan Ebrahimi <persionhost@gmail.com>
2024-04-29 22:11:21 +0330

    fix number format

--- resources/views/filament/marketer/resources/introduce-resource/widgets/total-comission.blade.php
+++ resources/views/filament/marketer/resources/introduce-resource/widgets/total-comission.blade.php
@@ -5,7 +5,17 @@
                 <x-heroicon-o-currency-dollar class="h-11"/>
             </div>
             <div class="flex-1">
-                {{ __('Dashboard_Balance' , ['price' => number_format(\App\Models\Introduce::query()
+                @php
+                    if (!function_exists('float_format')) {
+                        function float_format(float $num, ?string $decimal_separator = '.', ?string $thousands_separator = ','): string
+                        {
+                            $floorNum = floor($num);
+                            $digits = $num - $floorNum > 0 ? floatval( '0.'.\Illuminate\Support\Str::replaceFirst($floorNum.'.' , '' , $num) ) : 0;
+                            return number_format($floorNum , 0 , $decimal_separator ,$thousands_separator) . ( $digits > 0  ? $decimal_separator. \Illuminate\Support\Str::replaceFirst('0.' , '' , $digits) : '') ;
+                        }
+                    }
+                @endphp
+                {{ __('Dashboard_Balance' , ['price' => float_format(\App\Models\Introduce::query()
                                     ->where('is_earned' , false)
                                     ->where('user_id' , auth()->id())->sum('number_works_approved') * auth()->user()->commission_per_work )]) }}
             </div>
